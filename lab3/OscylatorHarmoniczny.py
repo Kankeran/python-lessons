eed8e48fa603faa89a1bb261222fd25bec34bcd8
kankeran <30869428+Kankeran@users.noreply.github.com>
2019-01-27 04:25:18 +0100

    Delete OscylatorHarmoniczny.py

--- lab3/OscylatorHarmoniczny.py
+++
@@ -1,7 +0,0 @@
-# • Wykonaj na wykresie
-# składającym się z dwóch
-# subplotów. Wykres obrazujący
-# oscylator tłumiony oraz oscylator
-# harmoniczny
-# • x(t)= cos(2pi*x1)*ex1
-# • X(t)=cos(2pi*x2)
